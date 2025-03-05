000ccadb88d9044a08caa65b157874667df7a0e1
ZackaryW <gitzackw@gmail.com>
2025-03-05 15:20:33 -0800

    ref: simplify builtin

--- src/zs/zflow/builtin.py
+++ src/zs/zflow/builtin.py
@@ -1,21 +1,18 @@
 import os
 
-class Builtin:
-    @staticmethod
-    def sleep(x: str):
-        from zuu.util_timeparse import time_parse
-        import datetime
-        sleep_till = time_parse(x)
-        elapsed = sleep_till - datetime.datetime.now()
-        if elapsed.total_seconds() > 0:
-            os.system(f"timeout /t {int(elapsed.total_seconds())}")
+def sleep(x: str):
+    from zuu.util_timeparse import time_parse
+    import datetime
+    sleep_till = time_parse(x)
+    elapsed = sleep_till - datetime.datetime.now()
+    if elapsed.total_seconds() > 0:
+        os.system(f"timeout /t {int(elapsed.total_seconds())}")
 
-    @staticmethod
-    def setenv(x: str):
-        k, v = x.split("=", 1)
-        os.environ[k] = v
+def setenv(x: str):
+    k, v = x.split("=", 1)
+    os.environ[k] = v
 
 BUILTIN_METHODS = {
-    "sleep": Builtin.sleep,
-    "setenv": Builtin.setenv,
+    "sleep": sleep,
+    "setenv": setenv,
 }
